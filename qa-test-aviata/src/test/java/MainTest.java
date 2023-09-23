ad12fa9db0f452bb962ecd8a09d74270033eef90
Mtnk11 <madinatawkenbai@gmail.com>
2023-09-23 12:51:30 +0600

    Add oneWay method

--- qa-test-aviata/src/test/java/MainTest.java
+++ qa-test-aviata/src/test/java/MainTest.java
@@ -1,15 +1,11 @@
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.Date;
-
-import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 
 public class MainTest {
@@ -21,6 +17,8 @@ public class MainTest {
                     .setSlowMo(1000))
             .newContext()
             .newPage();
+
+
     @BeforeEach
     public void test() throws InterruptedException {
         page.navigate("https://aviata.kz");
@@ -34,7 +32,7 @@ public class MainTest {
     @Test
     public void anywhere() {
         int n = 3;
-        page.click("//button[contains(text(),'Куда угодно')]");
+        page.click("//ul[1]//button[contains(text(),'Куда угодно')]");
         page.click("//span[contains(text(),'Любой месяц')]");
         page.click("//*[@id='search-route-0']/div[4]/div[1]/div[3]/div[2]/button[" + n + "]/div");
         page.click("//span[text()='Обратно']");
@@ -42,5 +40,13 @@ public class MainTest {
 
         page.screenshot(new Page.ScreenshotOptions()
                 .setPath(Paths.get("screens/" + LocalDate.now() + ".png")).setFullPage(true));
+    }
+    @Test
+    public void oneWay() {
+        page.click("//ul[1]//button[contains(text(),'В одну сторону')]");
+        page.locator("//div[1]/label/input").fill("Астана");
+        page.click("//div[1]/div[2]//span[1]/span[contains(text(),'Астана')]");
+        page.locator("//div[3]/label/input");
+        page.locator("//div[3]/label/input").fill("Кызылорда");
     }
 }
